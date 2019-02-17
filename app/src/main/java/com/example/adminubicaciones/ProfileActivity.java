4a62a30b83aaaf9880e56833d9b39ab43a579fca
ChristianCagide <ccagide1@gmail.com>
2019-02-17 14:10:01 -0600

    Agregada la funcionalidad de guardar los recordatorios para el usuario especifico en la BD

--- app/src/main/java/com/example/adminubicaciones/ProfileActivity.java
+++ app/src/main/java/com/example/adminubicaciones/ProfileActivity.java
@@ -1,26 +1,34 @@
 package com.example.adminubicaciones;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase database;
 
     private Button mButtonLogout;
 
-    private EditText mEditTextTitulo;
-    private EditText mEditTextMensaje;
+    private EditText mTextTitulo;
+    private EditText mTextMensaje;
 
     private Button mButtonCancelar;
     private Button mButtonAgregar;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +41,15 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             startActivity(new Intent(this, LoginActivity.class));
         }
 
+        database = FirebaseDatabase.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
         mButtonLogout = (Button)findViewById(R.id.button_logout);
         mButtonLogout.setOnClickListener(this);
 
-        mEditTextTitulo = (EditText)findViewById(R.id.edittext_titulo_record);
-        mEditTextMensaje = (EditText)findViewById(R.id.edittext_mensaje_record);
+        mTextTitulo = (EditText)findViewById(R.id.edittext_titulo_record);
+        mTextMensaje = (EditText)findViewById(R.id.edittext_mensaje_record);
 
         mButtonAgregar = (Button)findViewById(R.id.button_agregar);
         mButtonCancelar = (Button)findViewById(R.id.button_cancel);
@@ -54,6 +66,25 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private void agregarRecord(){
         //agregar recordatorio
+        final String titulo = mTextTitulo.getText().toString().trim();
+        final String mensaje = mTextMensaje.getText().toString().trim();
+
+        //Checar si los campos estan vacios
+        if(TextUtils.isEmpty(titulo)){
+            Toast.makeText(this,"Introduce un titulo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(mensaje)){
+            Toast.makeText(this,"Introduce un mensaje", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registrando usuario...");
+        progressDialog.show();
+
+        //Guardar recordatorio en la base de datos
+        DatabaseReference myRef = database.getReference(firebaseAuth.getCurrentUser().getUid());
+        myRef.child(titulo).setValue(mensaje);
         finish();
         startActivity(new Intent(this,DashboardActivity.class));
     }
